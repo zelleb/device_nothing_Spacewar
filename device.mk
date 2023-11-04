4ea3ea7e23f20a01efd62927fae962a22e23cc7b
aswin7469 <aswinas@pixysos.com>
2023-11-04 21:24:24 +0100

    Spacewar: build android.media.audio.common.types-V1-cpp
    * linker  : CANNOT LINK EXECUTABLE "/system_ext/bin/wfdservice": library "android.media.audio.common.types-V1-cpp.so" not found: needed by /system_ext/lib/libwfdservice.so in namespace
    
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- device.mk
+++ device.mk
@@ -339,7 +339,8 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
-    libavservices_minijail.vendor
+    libavservices_minijail.vendor \
+    android.media.audio.common.types-V1-cpp
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
