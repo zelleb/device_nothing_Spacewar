75e79f2ebba9af208a8697f16d5616c2614a6ab2
Luofan Chen <clfbbn@gmail.com>
2024-02-02 20:49:44 +0800

    Spacewar: Import QCC system_ext blobs
    Makes the QTI GNSS service happy. Thanks luca020400 for the help.
    
    Change-Id: I6de25b0b3fde6cce3a6cdf95f31c68734d429956
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,6 +121,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/pixys/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/framework_manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest_yupik.xml
 
