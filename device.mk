4b833e027e8da16d95f7e378025debe046da2d6d
aswin7469 <aswinas@pixysos.com>
2024-03-24 19:06:16 +0530

    Spacewar: Update to NOS 2.5.3
    * drop prebuilt libprotobuf-cpp-lite-3.9.1 as its now available on aosp
    
    [1] https://android.googlesource.com/platform/prebuilts/misc/+/e50c7362efa1580324ad9e12eb73859ba98b0dbf
    
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- device.mk
+++ device.mk
@@ -409,6 +409,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libqti-perfd-client
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.nothing-libperfmgr \
@@ -533,8 +538,3 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
-
-# Protobuf
-PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-3.9.1-vendorcompat
-
